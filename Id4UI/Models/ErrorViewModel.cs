3cce4419a1fbd3c5e15d5d7ca897a8cf48d78281
flysnoopy1984 <song_fuwei@126.com>
2020-03-27 16:47:39 +0800

    1

---
+++ Id4UI/Models/ErrorViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Id4UI.Models
+{
+    public class ErrorViewModel
+    {
+        private  string Error { get; set; }
+        public string _RequestId;
+        public ErrorViewModel()
+        {
+        }
+
+        public ErrorViewModel(string RequestId,string error)
+        {
+            _RequestId = RequestId;
+            Error = error;
+        }
+
+       
+    }
+}
